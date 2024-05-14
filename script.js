17b442656d44db3c5eb9b7671ffb2219fb20c9f2
James Blake <142352216+blakejam371@users.noreply.github.com>
2024-05-14 21:36:40 +0100

    Fix bugs with delete function and duplication

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 const myLibrary = [];
 
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', 'not read');
+const fellowshipOfTheRing = new Book('The Fellowship of the Ring', 'J.R.R Tolkien', '423', 'read');
+const twoTowers = new Book('The Two Towers', 'J.R.R Tolkien', '352', 'read');
+const aGameOfThrones = new Book('A Game of Thrones', 'George R. R. Martin', '694', 'read');
+
+myLibrary.push(theHobbit);
+myLibrary.push(fellowshipOfTheRing);
+myLibrary.push(twoTowers);
+myLibrary.push(aGameOfThrones);
+
+createBooks();
+
+let dialog = document.querySelector('dialog');
+let addNewBookButton = document.querySelector('.addNewBook');
+addNewBookButton.addEventListener('click', () => {
+  dialog.showModal();
+  let closeButton = document.querySelector('.closeButton');
+  closeButton.addEventListener('click', () => {
+    dialog.close();
+  })
+  let addToLibrary = document.querySelector('#addToLibrary');
+  addToLibrary.addEventListener('click', function(event) {
+    event.preventDefault();
+    addBookToLibrary();
+  });
+});
+
 function createBooks() {
+  document.querySelector('.bookcase').innerHTML = ''
+
   myLibrary.forEach(function(item) {
     let div = document.createElement('div');
     div.classList.add('books');
@@ -12,7 +41,7 @@ function createBooks() {
     author.textContent = item.author;
 
     let pages = document.createElement('p');
-    pages.textContent = item.pages;
+    pages.textContent = `${item.pages} pages`;
 
     let read = document.createElement('p');
     read.textContent = item.read;
@@ -22,7 +51,8 @@ function createBooks() {
     deleteButton.classList.add('deleteButton');
     deleteButton.addEventListener('click', function() {
       if(confirm('Are you sure you want to delete?')) {
-        deleteBook();
+        const index = myLibrary.indexOf(item);
+        deleteBook(index);
         div.remove();
       } else {
         return;
@@ -35,8 +65,10 @@ function createBooks() {
     readToggle.addEventListener('click', () => {
       if (read.textContent === 'read') {
         read.textContent = 'not read';
+        item.read = 'not read';
       } else {
         read.textContent = 'read';
+        item.read = 'read';
       }
     })
 
@@ -52,7 +84,10 @@ function createBooks() {
 };
 
 function deleteBook(index) {
-  myLibrary.splice(index, 1);
+  if (index >= 0 && index < myLibrary.length) {
+    myLibrary.splice(index, 1);
+    createBooks();
+  }
 }
 
 function Book(title, author, pages, read) {
@@ -62,30 +97,24 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-let dialog = document.querySelector('dialog');
-let addNewBookButton = document.querySelector('.addNewBook');
-addNewBookButton.addEventListener('click', () => {
-  dialog.showModal();
-})
-let closeButton = document.querySelector('.closeButton');
-closeButton.addEventListener('click', () => {
-  dialog.close();
-})
-
 function addBookToLibrary() {
-  // do stuff here
+  let title = document.querySelector('#bookName').value;
+  let author = document.querySelector('#authorName').value;
+  let pages = document.querySelector('#pageNumber').value;
+  let read = '';
+  const checkedRadioButton = document.querySelector('input[name="readToggle"]:checked');
+  if (checkedRadioButton) {
+    read = checkedRadioButton.value;
+    checkedRadioButton.checked = false;
+  }
 
-  // call Book() at end
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  createBooks();
+
+  document.querySelector('#bookName').value = '';
+  document.querySelector('#authorName').value = '';
+  document.querySelector('#pageNumber').value = '';
+
+  dialog.close();
 }
-
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295 pages', 'not read');
-const fellowshipOfTheRing = new Book('The Fellowship of the Ring', 'J.R.R Tolkien', '423 pages', 'read');
-const twoTowers = new Book('The Two Towers', 'J.R.R Tolkien', '352 pages', 'read');
-const aGameOfThrones = new Book('A Game of Thrones', 'George R. R. Martin', '694 pages', 'read');
-
-myLibrary.push(theHobbit);
-myLibrary.push(fellowshipOfTheRing);
-myLibrary.push(twoTowers);
-myLibrary.push(aGameOfThrones);
-
-createBooks();
